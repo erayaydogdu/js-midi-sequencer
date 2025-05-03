59a392241536edc04e48d24ec999266b3a810d49
erayaydogdu <eray.aydogdu@outlook.com>
2025-05-03 14:04:51 +0200

    deploy settings

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,16 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  output: 'export', // Add this line to enable static export
+  basePath: '/js-midi-sequencer', // Add this line with your repository name
+  assetPrefix: '/js-midi-sequencer/', // Add this line to ensure correct asset paths
+  reactStrictMode: true,
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
   images: {
     remotePatterns: [
       {
