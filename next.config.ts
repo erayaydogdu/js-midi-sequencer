0536f557e0c5d8902214b6c0d756bc9293fa17d8
home <eray.aydogdu@outlook.com>
2026-02-01 17:49:00 +0100

    update deployment

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // Static export for GitHub Pages
   distDir: 'out', // Output directory for static export
-  // basePath: '/js-midi-sequencer', // Add this line with your repository name
-  // assetPrefix: '/js-midi-sequencer/', // Add this line to ensure correct asset paths
+  basePath: '/js-midi-sequencer', // Repository name for GitHub Pages
+  assetPrefix: '/js-midi-sequencer/', // Ensure correct asset paths
   reactStrictMode: true,
   allowedDevOrigins: ['192.168.1.226', 'localhost', '*.local'],
   // typescript: {
